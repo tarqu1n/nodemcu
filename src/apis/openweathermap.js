a80fcada4e3c3057c536756df499e262b0978acd
Matthew Langley <matthew.langley@mirumagency.com>
2017-09-13 23:45:13 +0100

    update with test script

--- src/apis/openweathermap.js
+++ src/apis/openweathermap.js
@@ -9,7 +9,7 @@ class OpenWeatherMapApi {
 	}
 
 	request() {
-		require("https").get("https://api.openweathermap.org/data/2.5/weather?q=London", function(res) {
+		require("http").get("https://api.openweathermap.org/data/2.5/weather?q=London", function(res) {
 			console.log("here");
 			var contents = "";
 			res.on('data', function(data) {
